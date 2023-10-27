ffff7e1514a74dbbcc3a43c4a1dac581b1e94a0d
Gavin Loo <looweiren@gmail.com>
2023-10-28 02:01:23 +0800

    rendered stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { PointMaterial, Points, Preload } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { random } from 'maath'
+import React, { Suspense, useRef, useState } from 'react'
+
+function Stars(props) {
+  const ref = useRef()
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }))
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
 
 function App() {
   return (
-    <main className='min-h-screen min-h-screen bg-black text-white'>
+    <main className='min-h-screen min-h-screen bg-black text-white relative'>
       <div>App</div>
+      <div className='relative z-0 h-screen w-screen'>
+        <Canvas camera={{ position: [0, 0, 1] }}>
+          <Suspense fallback={null}>
+            <Stars />
+          </Suspense>
+          <Preload all />
+        </Canvas>
+      </div>
+      
     </main>
   )
 }
