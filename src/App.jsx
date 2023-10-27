64a6591d094db67327ee14ae83ba5ff4612ca87c
Gavin Loo <looweiren@gmail.com>
2023-10-28 02:08:45 +0800

    added animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { PointMaterial, Points, Preload } from '@react-three/drei'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { random } from 'maath'
-import React, { Suspense, useRef, useState } from 'react'
+import { PointMaterial, Points, Preload } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { random } from "maath";
+import React, { Suspense, useRef, useState } from "react";
 
 function Stars(props) {
-  const ref = useRef()
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }))
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -17,7 +19,7 @@ function Stars(props) {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#f272c8'
+          color="#f272c8"
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
@@ -29,9 +31,8 @@ function Stars(props) {
 
 function App() {
   return (
-    <main className='min-h-screen min-h-screen bg-black text-white relative'>
-      <div>App</div>
-      <div className='relative z-0 h-screen w-screen'>
+    <main className="min-h-screen min-h-screen bg-black text-white">
+      <div className="z-0 h-screen w-screen fixed">
         <Canvas camera={{ position: [0, 0, 1] }}>
           <Suspense fallback={null}>
             <Stars />
@@ -39,9 +40,19 @@ function App() {
           <Preload all />
         </Canvas>
       </div>
-      
+
+      <section className="min-h-screen flex items-center justify-center">
+        <div className="text-8xl">
+          <h1 className="animate__animated animate__fadeIn animate__slow">
+            hello
+          </h1>
+          <h1 className="animate__animated animate__fadeIn animate__slower">
+            world
+          </h1>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
